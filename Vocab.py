d724bb8933ee80590dea74f7983416b0807fe2e4
Bishal Santra <bsantraigi@gmail.com>
2018-08-14 11:41:57 +0530

    added vocab interface

---
+++ Vocab.py
@@ -0,0 +1,12 @@
+class Vocab:
+    def __init__():
+        return 0
+    def BuildVocab(trainingFiles):
+        return 0
+    def Write2Disk(folderPath):
+        return 0
+    def ReadVocab(VocabFile_Txt):
+        return 0
+    def DatasetReader(dataFiles):
+        ## Returns a iterator for sentences encoded by ids
+        return 0
